835b70b7bb799deb7a3e799c3650802c4677b972
Gregory Ditzler <ditzler@dhcp-inno-176-128.ROWAN.EDU>
2013-10-07 05:22:45 -0700

    script for making pcoa plots

--- hmp/hmp_pcoa.sh
+++ hmp/hmp_pcoa.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash 
 
-metrics=( hellinger unweighted_unifrac unifrac bray_curtis chisq euclidean weighted_unifrac ) 
+# Compute the princpal coordinates for the HMP data set. Run beta_diversity.py 
+# on the HMP data. The output of the analysis is the King data files that can 
+# be used to visualize the results from PCoA. 
+
+metrics=( hellinger unweighted_unifrac unifrac bray_curtis chisq euclidean \
+	weighted_unifrac ) 
 
 for metric in ${metrics[@]}; do 
 	echo "Submitting $metric"
@@ -9,3 +14,12 @@ for metric in ${metrics[@]}; do
 		-t ~/Git/data/hmp_rep_set.tre \
 		-m $metric & 
 done
+wait
+principal_coordinates.py -i ~/Git/data/hmp.pcoa/ -o ~/Git/data/hmp.beta/
+
+pcoa_files=`find ~/Git/data/hmp.beta/*.txt`
+for fname in ${pcoa_files[@]}; do 
+	make_3d_plots.py -i $fname -m ~/Git/data/hmp.map -o ~/Git/data/hmp.plots/ & 
+	make_2d_plots.py -i $fname -m ~/Git/data/hmp.map -o ~/Git/data/hmp.plots/ & 
+done
+wait 
